8fe1ed060a5af26ced996036125ed08a9d9a1454
Wowo Wibowo <wowow.908@gmail.com>
2024-05-20 17:13:15 +0700

    [FEATURE] - Init cms module and routing module
    * implement nested lazy loading for module cms

---
+++ src/app/modules/cms/cms-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CmsRoutingModule {}
---
+++ src/app/modules/cms/cms.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CmsRoutingModule } from './cms-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CmsRoutingModule],
+})
+export class CmsModule {}
